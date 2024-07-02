f00240aa7f51d67c271eba9629557f3d5cb245bc
naim195 <119221365+naim195@users.noreply.github.com>
2024-07-02 22:25:58 +0530

    added vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { inject } from '@vercel/analytics';
+import { Analytics } from "@vercel/analytics/react"
  
-inject();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
+    <Analytics/>
   </React.StrictMode>,
 );
